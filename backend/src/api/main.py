0ed75ba1b4e264f2544a668a23dc08dac5041f58
rajiv.gupta <rajiv.gupta@acldigital.com>
2024-06-19 13:18:38 +0530

    Minor changes in folder Heirarchy

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -37,4 +37,4 @@ async def read_root():
 # Run the application with: uvicorn app.main:app --reload
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
